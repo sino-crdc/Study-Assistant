2fb634a55fe7b95f7369e268fab0e578fadef52d
zzzsyyy <zsy3141592@163.com>
2021-07-26 22:41:57 +0800

    add: add request.js as Promise

--- MiniProgram/dev/pages/vocsearch/vocsearch.js
+++ MiniProgram/dev/pages/vocsearch/vocsearch.js
@@ -1,33 +1,33 @@
+import { request } from "../../request/request.js"
+
 Page({
   data: {
-    value: '',
-    resultList:[],
+    value:'',
+    resultList:[]
   },
   onChange(e) {
     this.setData({
-      value: e.detail,
+      value: e.detail
     });
   },
   onSearch() {
-    wx.request({
-      url: 'https://api.zzzsy.top/xxx/xxx',
-      data:{value},
-      success: (result) => {
-        this.setData({
-          resultList:result.message.resultList
-        });
-      }
+    request({url: 'https://api.zzzsy.top/xxx/xxx',
+             data: value
+    })
+    .then(result =>{
+      this.setData({
+        resultList:result.message.resultList
+      })
     });
   },
   onClick() {
-    wx.request({
-      url: 'https://api.zzzsy.top/xxx/xxx',
-      data:{value},
-      success: (result) => {
-        this.setData({
-          resultList:result.message.resultList
-        });
-      }
+    request({url: 'https://api.zzzsy.top/xxx/xxx',
+             data: {value}
+    })
+    .then(result =>{
+      this.setData({
+        resultList:result.message.resultList
+      })
     });
   },
   onShareAppMessage() {
