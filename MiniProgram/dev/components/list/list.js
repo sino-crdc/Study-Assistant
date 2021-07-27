82814caad74594953cf0fb3186308ce55a6763e6
zzzsyyy <zsy3141592@163.com>
2021-07-27 21:42:14 +0800

    add: add rusultlist component

---
+++ MiniProgram/dev/components/list/list.js
@@ -0,0 +1,29 @@
+Component({
+  behaviors: [],
+  properties: {
+    resultList:{
+      type:Array,
+      value:[]
+    }
+  },
+  data: {
+
+  },
+  lifetimes: {
+    created() {
+
+    },
+    attached() {
+
+    },
+    moved() {
+
+    },
+    detached() {
+
+    },
+  },
+  methods: {
+
+  },
+});
--- MiniProgram/dev/request/request.js
+++ MiniProgram/dev/request/request.js
@@ -10,4 +10,4 @@ export const request=(params)=>{
           }
         });
     })
-}
+} 
