e2c40f573983aaa213cf3ed29aa0d9b8b7d0d22a
zzzsyyy <zsy3141592@163.com>
2021-08-01 20:49:51 +0800

    beautify: page welcome

--- MiniProgram/dev/pages/profile/profile.js
+++ MiniProgram/dev/pages/profile/profile.js
@@ -2,10 +2,6 @@ Page({
   data: {
     avatarUrl:"../../assets/images/avatar.png"
   },
-  onGetUserInfo(e){
-    const {userInfo}=e.detail;
-    wx.setStorageSync('userinfo', userInfo)
-  },
   onLogin(){
     wx.navigateTo({
       url: '../welcome/welcome',
--- MiniProgram/dev/pages/welcome/welcome.js
+++ MiniProgram/dev/pages/welcome/welcome.js
@@ -2,6 +2,11 @@ Page({
   data: {
 
   },
+  onGetUserProfile(e){
+    const {userInfo}=e.detail;
+    wx.setStorageSync('userinfo', userInfo);
+    console.log(userInfo);
+  }, 
   onClick(){
     wx.switchTab({
       url: '../index/index',
