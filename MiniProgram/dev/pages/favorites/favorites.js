fb8882a5ed76d8f26f95198f670a1485c5af8e14
zzzsyyy <zsy3141592@163.com>
2021-08-13 17:09:29 +0800

    feat: update page Fav chore: improve search

--- MiniProgram/dev/pages/favorites/favorites.js
+++ MiniProgram/dev/pages/favorites/favorites.js
@@ -1,8 +1,38 @@
+import { request } from "../../request/request.js";
+
 Page({
   data: {
-
+    edit: true,
+    userFav: [],
+    flag: false,
+    netErr: false
   },
   onLoad(options) {
-
+    request({url: "/favorites",
+          // data: {"user_id": ""}
+    }).then(
+        (res)=>{
+          this.setData({userFav: res.data.data.userFav});
+          this.setData({netErr: false, flag: false});
+        },()=>{
+          console.log("err");
+          this.setData({netErr: true});
+        }).then(
+          ()=>{
+            this.setData({loading: false});
+            if (!this.data.userFav && !netErr){
+              this.setData({flag: true});
+            }
+        })
+  },
+  onEdit(){
+    var edit = this.data.edit;
+    this.setData({edit: !edit})
+  },
+  onDetail(e){
+    var voc = e.currentTarget.dataset.voc;
+    wx.navigateTo({
+      url: '../vocdetail/vocdetail?voc=' + voc,
+    })
   }
 });
--- MiniProgram/dev/pages/vocsearch/vocsearch.js
+++ MiniProgram/dev/pages/vocsearch/vocsearch.js
@@ -1,6 +1,4 @@
-import {
-  request
-} from "../../request/request.js";
+import { request } from "../../request/request.js";
 
 Page({
   data: {
@@ -10,11 +8,11 @@ Page({
     netErr: false,
     schErr: false,
   },
+
   onChange(e) {
-    this.setData({
-      keyword: e.detail.value,
-    });
+    this.setData({keyword: e.detail.value});
   },
+
   onSearch() {
     var keyword = this.data.keyword;
     this.search(keyword);
@@ -22,48 +20,37 @@ Page({
   onClick() {
     this.onSearch();
   },
+
   onLoad(options) {
     var keyword = options.key;
-    this.setData({
-      keyword,
-    });
+    this.setData({keyword});
     this.search(keyword);
   },
 
   search(keyword) {
     console.log("sch");
-    this.setData({
-      loading: true,
-    });
-    request({
-        url: "/search",
+    this.setData({resultList: []});
+    this.setData({loading: true});
+    request({url: "/search",
         //  data: {"keyword": keyword}
       })
       .then(
         (res) => {
-          this.setData({
-            resultList: res.data.data.resultList,
-          });
+          this.setData({resultList: res.data.data.resultList});
           this.setData({
             netErr: false,
-            schErr: false
+            schErr: false,
           });
         },
         () => {
           console.log("err");
-          this.setData({
-            netErr: true,
-          });
+          this.setData({netErr: true});
         }
       )
       .then(() => {
-        this.setData({
-          loading: false,
-        });
+        this.setData({loading: false});
         if (!this.data.resultList && !netErr) {
-          this.setData({
-            schErr: true,
-          });
+          this.setData({schErr: true});
         }
       });
   },
