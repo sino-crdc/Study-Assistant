d82b0055feca8842d5ddf38b92ff1a09f985f2c9
zzzsyyy <zsy3141592@163.com>
2021-08-15 20:34:00 +0800

    feat: update page Fav

--- MiniProgram/dev/pages/favorites/favorites.js
+++ MiniProgram/dev/pages/favorites/favorites.js
@@ -3,20 +3,46 @@ import { request } from "../../request/request.js";
 Page({
   data: {
     management: false, //控制管理条目（即复选框是否显示）
-    userFav: [], //条目列表
     no_fav: false, //控制没有条目的提示
     netErr: false, //控制网络错误的提示
     loading: true, //控制是否正在加载的提示
     select_all: false, //是否全选
     userFav_management: [],//与checkbox关联的条目列表
+    checked_num: 0
   },
   onLoad(options) {
+    if (wx.getStorageSync('userFav').length == 0){
+        this.sync();
+    }else{
+      request({
+        url: 'favorites',
+        data: {
+          "status": "compare",
+          "data": wx.getStorageSync('userFav_token'),
+          "user_id": user_id,
+        }
+      }).then((res)=>{
+        if (res.data.code=600){
+          this.sync();
+        }else{
+          this.submit();
+        }
+      })
+      this.setData({loading: false});
+      this.init();
+    }
+  },
+  sync:function(){
     request({
       url: "/favorites",
-      // data: {"user_id": ""}
+      // method: "POST",
+      // data: {
+        // "status": "sync",
+        // "user_id": user_id,
+      // }
     }).then(
       (res) => {
-        this.setData({ userFav: res.data.data.userFav });
+        wx.setStorageSync('userFav', res.data.data.userFav);
         this.setData({
           netErr: false,
           no_fav: false,
@@ -35,7 +61,7 @@ Page({
         this.setData({
           loading: false
         });
-        if (this.data.userFav.length == 0 && !this.data.netErr) {
+        if (wx.getStorageSync('userFav').length == 0 && !this.data.netErr) {
           this.setData({
             no_fav: true
           });
@@ -53,7 +79,7 @@ Page({
     this.setData({
       management: false
     });
-    this.init();
+    this.select_none();
   },
   //点击跳转详情
   onDetail(e) {
@@ -63,12 +89,13 @@ Page({
       url: '../vocdetail/vocdetail?voc=' + voc,
     })
   },
-  init(){
-    var arr = this.data.userFav;
+  init:function(){
+    var arr = wx.getStorageSync('userFav');
     var arr2 = [];
     arr.map(((item)=>{
       arr2.push(Object.assign({},item,{checked: false}))
     }));
+    console.log('init');
     console.log(arr2);
     this.setData({userFav_management: arr2});
   },
@@ -79,8 +106,13 @@ Page({
       return;
     } else {
       var arr = _ts.data.userFav_management;
-      var index = e.currentTarget.dataset.id;
+      var index = e.currentTarget.dataset.idx;
       arr[index].checked = !arr[index].checked;
+      if (arr[index].checked){
+        _ts.setData({checked_num: _ts.data.checked_num+1});
+      }else{
+        _ts.setData({checked_num: _ts.data.checked_num-1});
+      };
       console.log(arr);
       _ts.setData({ userFav_management: arr });
     }
@@ -88,6 +120,48 @@ Page({
   //删除条目
   delete:function(){
     const _ts = this;
-    var 
+    var arr = _ts.data.userFav_management;
+    var arr2 = [];
+    var arr3 = wx.getStorageSync('userFav');
+    var arr4 = [];
+    for (let i=0;i<arr.length;i++){
+      if (arr[i].checked == false){
+        arr2.push(arr[i]);
+        arr4.push(arr3[i]);
+      }
+    };
+    wx.setStorageSync('userFav', arr4);
+    _ts.setData({userFav_management: arr2, checked_num: 0});
+    this.submit();
+  },
+  //全选
+  select_all:function(){
+    var arr = this.data.userFav_management;
+    for (let i=0;i<arr.length;i++){
+      arr[i].checked = true
+    };
+    this.setData({userFav_management: arr, checked_num: arr.length});
+  },
+  //取消全选
+  select_none:function(){
+    var arr = this.data.userFav_management;
+    for (let i=0;i<arr.length;i++){
+      arr[i].checked = false
+    };
+    this.setData({userFav_management: arr, checked_num: 0});
+  },
+  //向服务器提交并获取token
+  submit:function(){
+    var arr = wx.getStorageSync('userFav');
+    request({
+      url: "/favorites",
+      data: {
+        "status": "submit",
+        "data": arr,
+        "user_id": user_id,
+      }
+    }).then((res)=>{
+      wx.setStorageSync('userFav_token', res.data.token);
+    })
   }
 });
