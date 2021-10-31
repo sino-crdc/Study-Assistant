228128d80b8d25c0077a0d4f9cd04f26763772a9
zzzsyyy <zsy3141592@163.com>
2021-10-31 14:45:34 +0800

    fix: 优化添加页面

--- MiniProgram/dev/pages/addEntry/addEntry.js
+++ MiniProgram/dev/pages/addEntry/addEntry.js
@@ -3,8 +3,11 @@ import { navTo } from "../../utils/common";
 import Dialog from '../../components/vant/dialog/dialog';
 import { request } from "../../utils/request";
 
+const app = getApp();
+
 Page({
   data: {
+    detail:[],
     llist: {
       title: "",
       chinese: "",
@@ -16,15 +19,17 @@ Page({
     },
     show: {
       title: true,
-      chinese: true,
-      content: true,
-      proof: true,
-      remark: true,
-      example: true,
+      chinese: false,
+      content: false,
+      proof: false,
+      remark: false,
+      example: false,
       source: true,
     },
   },
-  onLoad() {},
+  onShow() {
+    this.setTowxml()
+  },
   isShow: function (e) {
     var t = e.currentTarget.id;
     var s = this.data.show[t];
@@ -33,19 +38,12 @@ Page({
       [r]: !s,
     });
   },
-  //!Doing
+  //?Doing
   onEdit(e) {
     var type = e.currentTarget.id;
     const _ts = this;
     const success_callback = (res) => {
-      if (type == "title") {
-        res.eventChannel.emit(
-          "onL",
-          _ts.data.llist.title + "\n" + _ts.data.llist.chinese
-        );
-      } else {
-        res.eventChannel.emit("onL", _ts.data.llist[type]);
-      }
+      res.eventChannel.emit("onL", _ts.data.llist[type]);
     };
     navTo(
       {
@@ -94,5 +92,38 @@ Page({
       Toast({ message: "有必填项未填！", position: "bottom" });
       return false
     }
-  }
+  },
+  //?Doing
+  setTowxml() {
+    // const pageState = pageStates(this);
+    const { theme } = wx.getSystemInfoSync();
+    const detail = this.data.llist;
+    let title = detail.title;
+    let content = app.towxml(detail.content, "markdown", {
+      theme: theme,
+    });
+    let proof = app.towxml(detail.proof, "markdown", {
+      theme: theme,
+    });
+    let remark = app.towxml(detail.remark, "markdown", {
+      theme: theme,
+    });
+    let example = app.towxml(detail.example, "markdown", {
+      theme: theme,
+    });
+    let source = detail.source;
+    let chinese = detail.chinese;
+    let author = detail.author;
+    this.setData({
+      "detail.title": title,
+      "detail.content": content,
+      "detail.proof": proof,
+      "detail.remark": remark,
+      "detail.example": example,
+      "detail.source": source,
+      "detail.chinese": chinese,
+      "detail.author": author,
+    });
+    // pageState.finish();
+  },
 });
